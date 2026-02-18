f725d2772bc7c606268353c1b24dc276ab4c8040
Henrico <henry_gianello@estudante.sesisenai.org.br>
2026-02-18 20:45:52 +0000

    update

--- Atividade Python/Atividade3.py
+++ Atividade Python/Atividade3.py
@@ -1,1 +1,7 @@
-nome = input()
+nome = input("insra seu nome: ")
+valor1 = float(input("insira o primeiro valor: "))
+valor2 = float(input("insira o Segundo valor: "))
+print("Ola, meu nome é ", nome, "tenho o valor: ", valor1+valor2, "a receber")
+
+print(f"Ola, meu nome é {nome} tenho o valor:  {valor1+valor2} a receber")
+
